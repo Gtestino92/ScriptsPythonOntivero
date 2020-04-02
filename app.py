e88b937bd9b67ceecee28e7a5077568c1747455a
Giuliano <gtestino92@hotmail.com>
2020-04-01 23:33:44 -0300

    tiro bad request en caso de error

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from resultPedidosMlibreMock import pedidos
 from getPedidos import makePedidosFromXlsx
 app = Flask(__name__)
 
 @app.route("/pedidosEntregadosML", methods = ['POST'])
 def pedidosEntregadosML():
-    pedidosXlsxFile = request.files['pedidosEntregados']
-    return str(makePedidosFromXlsx(pedidosXlsxFile))
-    
+    try:
+        pedidosXlsxFile = request.files['pedidosEntregados']
+        return str(makePedidosFromXlsx(pedidosXlsxFile))
+    except(Exception):
+        return Response(status=400)
+
 if(__name__ == "__main__"):
     app.run(debug=True, port=4000) 
