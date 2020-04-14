a4e05c3e1916fda124637045b2697e462fd769f3
Giuliano <gtestino92@hotmail.com>
2020-04-14 00:26:27 -0300

    traigo fecha de mongodb

--- pedidosMongoService.py
+++ pedidosMongoService.py
@@ -1,9 +1,19 @@
 from mongoConnection import mongo
+import pandas as pd
 
 def getPedidosEntregados():
-    pedidosDb = mongo.db
-    print(pedidosDb)
-    pedidos = pedidosDb['pedidos']
+    ontiveroDb = mongo.db
+    pedidos = ontiveroDb['pedidos']
+    pedidosInfo = ontiveroDb['pedidos_info']
+    listPedidosInfo = list(pedidosInfo.find({}))
+    dfPedidosInfo = pd.DataFrame(listPedidosInfo)
+    pedidosFecha = dfPedidosInfo['fecha_entrega']
+    pedidosFecha.dropna(inplace=True)
+    print(pedidosFecha.head())
     listPedidos = list(pedidos.find({}))
-    print(listPedidos)
+    dfPedidos = pd.DataFrame(listPedidos)
+    dfPedidos.drop('_id', axis=1, inplace=True)
+    dfPedidos.set_index("id_pedido", inplace=True)
+    dfPedidos.fillna(0, inplace=True)
+    print(dfPedidos.head())
     return listPedidos
