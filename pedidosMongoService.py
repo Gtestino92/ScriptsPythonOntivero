e7553c0934f3b3cae99769854ac110112863818a
Giuliano <gtestino92@hotmail.com>
2020-04-19 22:17:48 -0300

    obtengo dict con formatos

--- pedidosMongoService.py
+++ pedidosMongoService.py
@@ -7,15 +7,24 @@ def getPedidosEntregados():
     dfPedidos = getPedidos(ontiveroDb)
     dfPedidosFull = pd.merge(left=dfPedidos, right=dfPedidosFecha, how='left', left_index=True, right_index=True)
     dfPedidosFull.dropna(inplace=True)
-    print(dfPedidosFull)
+    dfPedidosFull.to_excel("fileOutput.xlsx")  
+    formatDict = getFormatDict()
+    print(formatDict["001"].formato)
     return []
+
+def getFormatDict():
+    fileInfo = open("macetasInfo.xlsx", "rb")
+    dfsInfo = pd.read_excel(fileInfo, header=0, sheet_name='info')
+    dfsInfo = dfsInfo[["codigo nuevo","formato"]]
+    dfsInfo.set_index("codigo nuevo", inplace = True)
+    return dfsInfo.T.to_dict('series')
 
 def getFechas(db):
     pedidosInfo = db['pedidos_info']
     listPedidosInfo = list(pedidosInfo.find({}))
     dfPedidosInfo = pd.DataFrame(listPedidosInfo)
     dfPedidosFecha = dfPedidosInfo[['fecha_entrega','id_pedido']]
-    dfPedidosFecha.dropna(inplace=True)
+    dfPedidosFecha = dfPedidosFecha.dropna()
     dfPedidosFecha.set_index("id_pedido", inplace=True)
     dfPedidosFecha.columns=['fecha']
     return dfPedidosFecha
